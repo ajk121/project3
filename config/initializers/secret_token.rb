283fc301f522adcc286761932b619cf92a849f74
ajk121 <contact.abdulkhan@gmail.com>
2014-10-10 10:41:48 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CareConnect::Application.config.secret_token = '94d8d16d03ca67f7db8c59768f393247b03dacfae129011928f58b8195443f0fa63e06260d290262faf564737b545f14ff38ceb2de1dd5542e24d2b63b05a69f'
