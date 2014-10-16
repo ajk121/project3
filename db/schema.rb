eb069015fef2f769a5bfcc2455f11d84eea246d4
ajk121 <contact.abdulkhan@gmail.com>
2014-10-16 21:16:34 +0100

    location added to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141016154855) do
+ActiveRecord::Schema.define(:version => 20141016201555) do
 
   create_table "attendees", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20141016154855) do
     t.date     "date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "location"
   end
 
   create_table "messages", :force => true do |t|
